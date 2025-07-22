6cb602c54b3d1fd7af6860bacd705e43f6ee322c
nyagajjr <nyagaben123@gmail.com>
2025-07-22 13:56:24 +0200

    innerText vs textContent

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
-// document.getElementById("count-el").innerText = 6
+const add = document.querySelector("#count-el")
 const txt = document.querySelector("#my_calculator")
 const err = document.querySelector("#Message")
 const msg = document.querySelector("#bjack")
-
+const saver = document.querySelector("#save-message")
+let count = 0
 
 console.log(err)
 console.log(msg)
 
 function danger(){
-    err.textContent = "Click my calculator button below"
+    count += 1
+    add.textContent = count
+    err.textContent = "Don't ignore my calculator button below"
     
 }
 
@@ -18,4 +21,11 @@ function next(){
 
 function info(){
     msg.textContent = "Coming soon!"
+}
+
+function save(){
+    let str_inc = count + " - "
+    saver.textContent += str_inc
+    add.textContent = 0
+    count -= count
 }

12fef5a5cdfd44cb11d90e200e97d8e56f972fff
nyagajjr <nyagaben123@gmail.com>
2025-07-22 15:28:19 +0200

    Adding more logics to the game

--- js/bjack.js
+++ js/bjack.js
@@ -1,5 +1,31 @@
-const msg = document.querySelector("#bjack")
+let firstCard = 10
+let secondCard = 1
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = "" 
+let messageEl = document.querySelector("#message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
-function msg(){
-    msg.textContent = "Coming soon!"
+
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+
+function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+
 }
+// startGame()
+
